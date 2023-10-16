bb68003d2bbe8a2e0ca612b16f786f0a13dc85ad
Tanish Makadia <tanishmakadia@gmail.com>
2023-10-16 17:50:21 -0400

    chore: Add button link test page to routing config

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  exportPathMap: async function() {
+    return {
+      "/button-link-test": { page: "/button-link-test" },
+    };
+  },
 }
 
 module.exports = nextConfig
