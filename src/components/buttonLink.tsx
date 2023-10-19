5f9bec3fc1d9760da9a65eef0d7d016ae2e7c967
Tanish Makadia <tanishmakadia@gmail.com>
2023-10-19 16:46:56 -0400

    feat: Add transition betweendefault and hover state

--- src/components/buttonLink.tsx
+++ src/components/buttonLink.tsx
@@ -10,7 +10,7 @@ const ButtonLink = (props: Props) => {
   return (
     <div>
         <Link href={props.url}>
-            <button className="font-sans font-normal text-slate-200 rounded-lg border-2 border-slate-200 m-3 py-1 px-3.5">{props.text}</button>
+            <button className="font-sans font-normal text-slate-200 rounded-lg hover:rounded-none ease-out duration-100 border-2 border-slate-200 m-3 py-1 px-3.5">{props.text}</button>
         </Link>
     </div>
   )
