b312d77a2cdca5ded27db65e530132fb09ff3eb5
Tanish Makadia <tanishmakadia@gmail.com>
2023-10-19 21:16:04 -0400

    feat: Change button background color upon hover

--- src/components/buttonLink.tsx
+++ src/components/buttonLink.tsx
@@ -10,7 +10,7 @@ const ButtonLink = (props: Props) => {
   return (
     <div>
         <Link href={props.url}>
-            <button className="font-sans font-normal text-slate-200 rounded-lg hover:rounded-none ease-out duration-100 border-2 border-slate-200 m-3 py-1 px-3.5">{props.text}</button>
+            <button className="font-sans font-normal hover:bg-slate-200 hover:text-black text-slate-200  ease-out duration-100 border-2 border-slate-200 m-3 py-1 px-3.5">{props.text}</button>
         </Link>
     </div>
   )
