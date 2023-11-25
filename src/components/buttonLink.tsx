2b3d587d8453a6ebb32cd3009f1b379e53552341
Tanish Makadia <tanishmakadia@gmail.com>
2023-11-25 11:48:32 -0500

    fix: Remove colors from buttonLink props since dynamic styling does not work

--- src/components/buttonLink.tsx
+++ src/components/buttonLink.tsx
@@ -4,20 +4,18 @@ import Link from "next/link";
 type Props = {
     text: string;
     url: string;
-    color1: string;
-    color2: string;
 };
 
 const ButtonLink = (props: Props) => {
   return (
     <Link href={props.url} className="group bg-transparent">
-      <button className={`font-sans font-normal bg-transparent hover:bg-${props.color1} text-${props.color1} hover:text-${props.color2} border-2 border-${props.color1} hover:border-transparent rounded ease-out duration-300 py-1.5 px-6`}>
+      <button className="font-sans font-normal bg-transparent hover:bg-medium-brown text-medium-brown hover:text-light-brown border-2 border-medium-brown hover:border-transparent rounded ease-out duration-300 py-1.5 px-6">
         <text className="text-xl bg-transparent">{props.text}</text>
         <span className="mx-1"/>
-        <span className={`text-xl inline-block bg-transparent transition ease-in-out group-hover:translate-x-3 text-${props.color1} group-hover:text-${props.color2} duration-300`}>→</span>
+        <span className="text-xl inline-block bg-transparent transition ease-in-out group-hover:translate-x-3 text-medium-brown group-hover:text-light-brown duration-300">→</span>
       </button>
     </Link>
-  )
+  );
 };
 
 export default ButtonLink;
--- src/components/qualtricsButton.tsx
+++ src/components/qualtricsButton.tsx
@@ -1,14 +1,9 @@
 import ButtonLink from "@/components/buttonLink";
 import React from "react";
 
-type Props = {
-    color1: string;
-    color2: string;
-};
-
-const QualtricsButton = (props: Props) => {
+const QualtricsButton = () => {
     return (
-        <ButtonLink text="Qualtrics Survey" url="https://brown.co1.qualtrics.com/jfe/form/SV_2seiVuFl1tCmNim" color1={props.color1} color2={props.color2}/>
+        <ButtonLink text="Qualtrics Survey" url="https://brown.co1.qualtrics.com/jfe/form/SV_2seiVuFl1tCmNim"/>
     )
 };
 
