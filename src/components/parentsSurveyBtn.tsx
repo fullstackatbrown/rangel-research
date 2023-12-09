4166cbb407893de6769ce532e952b9146467711f
Julie Wang <juliewang@julies-mbp-430.devices.brown.edu>
2023-12-09 11:42:03 -0500

    started about page

---
+++ src/components/parentsSurveyBtn.tsx
@@ -0,0 +1,13 @@
+import ButtonLink from "@/components/buttonLink";
+import React from "react";
+
+const QualtricsButton = () => {
+  return (
+    <ButtonLink
+      text="PARENTS SURVEY"
+      url="https://brown.co1.qualtrics.com/jfe/form/SV_2seiVuFl1tCmNim"
+    />
+  );
+};
+
+export default QualtricsButton;
